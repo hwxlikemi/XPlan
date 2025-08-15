6d7ff24cc8d8cd9cb37a0718307a8fd0139dd312
ItosEO <m2265514616@163.com>
2025-08-15 21:45:35 +0800

    feat(app): 更新应用版本并添加新功能
    - 将应用版本从 2.1 升级到 2.3
    - 更新版本号和版本名称
    - 添加对 ColorOS、Meizu、Samsung 和 Vivo 系统的新功能支持
    - 新增多个系统应用的优化和适配

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.itos.xplan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.1"
+        versionCode = 14
+        versionName = "2.3"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
